7c6cb2d764dbe804a2cec02c7e34fb43fe0c909c
kim-do-hyeon <pental@kakao.com>
2022-05-23 23:17:33 +0900

    How to detect horizontial planes in the real world

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -40,14 +42,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         /* 368. How to Find and Import 3D Models for AR */
         // Create a new scene
-        let dicescene = SCNScene(named:"art.scnassets/diceCollada.scn")!
+//        let dicescene = SCNScene(named:"art.scnassets/diceCollada.scn")!
+//
+//        if let diceNode = dicescene.rootNode.childNode(withName: "Dice", recursively: true){
+//
+//            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//
+//            sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
         
-        if let diceNode = dicescene.rootNode.childNode(withName: "Dice", recursively: true){
-                
-            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-            
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        }
         
     }
     
@@ -56,6 +59,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        /* 369. How to Detect Horizontial Planes in the Real World */
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -66,4 +73,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            print("Plane Detected")
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0.1, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+            
+            
+        } else{
+            return
+        }
+    }
+    
 }
