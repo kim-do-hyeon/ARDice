d6fd3220e338446da3bb12ef246541295cb3dfab
kim-do-hyeon <pental@kakao.com>
2022-05-28 14:40:48 +0900

    Refactoring our code for better readability

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        // 375. Refactoring Our Code for Better Readability
+        // self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -77,6 +78,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    
+    //MARK : ARSCNViewDelegateMethods
+    
+    
     /* 370. How to Detect Touch on Real World 3D Objects */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first{
@@ -92,37 +97,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             /* 371. How to Place Our Dice in 3D Using */
             if let hitResult = results.first{
-                
-                // Create a new scene
-                let dicescene = SCNScene(named:"art.scnassets/diceCollada.scn")!
-        
-                if let diceNode = dicescene.rootNode.childNode(withName: "Dice", recursively: true){
-        
-                    diceNode.position = SCNVector3(
-                        x: hitResult.worldTransform.columns.3.x,
-                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
-                        z: hitResult.worldTransform.columns.3.z
-                    )
-        
-                    // 373. How to Animate and Roll all the 3D Dice at Once
-                    diceArray.append(diceNode)
-                    
-                    sceneView.scene.rootNode.addChildNode(diceNode)
-                    
-                    roll(dice: diceNode)
-                }
+                addDice(atLocation: hitResult)
             }
         }
     }
     
-    // 373. How to Animate and Roll all the 3D Dice at Once
-    func rollAll(){
-        if !diceArray.isEmpty{
-            for dice in diceArray{
-                roll(dice: dice)
-            }
+    // 375. Refactoring Our Code for Better Readability
+    func addDice(atLocation location : ARHitTestResult){
+        // Create a new scene
+        let dicescene = SCNScene(named:"art.scnassets/diceCollada.scn")!
+
+        if let diceNode = dicescene.rootNode.childNode(withName: "Dice", recursively: true){
+
+            diceNode.position = SCNVector3(
+                x: location.worldTransform.columns.3.x,
+                y: location.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                z: location.worldTransform.columns.3.z
+            )
+
+            // 373. How to Animate and Roll all the 3D Dice at Once
+            diceArray.append(diceNode)
+            
+            sceneView.scene.rootNode.addChildNode(diceNode)
+            
+            roll(dice: diceNode)
         }
     }
+    
     
     func roll(dice: SCNNode){
         /* 372. How to Animate 3D Objects in AR */
@@ -139,6 +140,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         )
     }
     
+    
+    // 373. How to Animate and Roll all the 3D Dice at Once
+    func rollAll(){
+        if !diceArray.isEmpty{
+            for dice in diceArray{
+                roll(dice: dice)
+            }
+        }
+    }
+    
+
     // 373. How to Animate and Roll all the 3D Dice at Once
     @IBAction func rollAgain(_ sender: UIBarButtonItem) {
         rollAll()
@@ -157,34 +169,46 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
+    //MARK : ARSCNViewDelegateMethods
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor is ARPlaneAnchor {
-            print("Plane Detected")
-            let planeAnchor = anchor as! ARPlaneAnchor
-            
-            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-            
-            let planeNode = SCNNode()
-            
-            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0.1, z: planeAnchor.center.z)
-            
-            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
-            
-            let gridMaterial = SCNMaterial()
-            
-            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
-            
-            plane.materials = [gridMaterial]
-            
-            planeNode.geometry = plane
-            
-            node.addChildNode(planeNode)
-            
-            
-            
-        } else{
-            return
-        }
+        guard let planeAnchor = anchor as? ARPlaneAnchor else{return}
+        
+        let planeNode = createPlane(withPlaneAnchor: planeAnchor)
+        
+        node.addChildNode(planeNode)
+        
+//        if anchor is ARPlaneAnchor {
+//
+//            print("Plane Detected")
+//
+//            let planeAnchor = anchor as! ARPlaneAnchor
+//
+//        } else{
+//            return
+//        }
+//    }
+    }
+    
+    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        
+        let planeNode = SCNNode()
+        
+        planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0.1, z: planeAnchor.center.z)
+        
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        
+        let gridMaterial = SCNMaterial()
+        
+        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+        
+        plane.materials = [gridMaterial]
+        
+        planeNode.geometry = plane
+        
+        return planeNode
     }
     
 }
+
